184614e72abc0fa0a1fe0cfa80a3f8a45c4faae3
Norisama <gloriazbh@gmail.com>
2023-09-04 19:21:08 -0400

    done frontend

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,9 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="flex justify-between items-center bg-slate-800 px-6 py-3 shadow-md">
+        <nav className="flex justify-between items-center px-6 py-3 shadow-md h-32"
+            style={{ backgroundImage: "url('/navbar-img.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}
+        >
             <Link className="text-white font-bold text-2xl hover:text-slate-400 transition-colors duration-300" href={"/"}>Movie Magic</Link>
             <Link className="bg-slate-700 text-white p-2 rounded hover:bg-slate-600 transition-colors duration-300" href={"/addMovie"}>Add Movie</Link>
         </nav>
