83c03fb3209e790a233392b7cd8a1fbd2bedfefb
Norisama <gloriazbh@gmail.com>
2023-09-04 18:15:43 -0400

    update jsx components style

--- app/components/TopicsList.jsx
+++ app/components/TopicsList.jsx
@@ -1,23 +1,25 @@
 import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
-import {HiPencilAlt} from "react-icons/hi";
+import { HiPencilAlt } from "react-icons/hi";
 
 export default function TopicsList() {
     return (
         <>
-            <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"> 
+            <div className="p-5 border border-slate-200 my-4 flex justify-between gap-4 items-center bg-white shadow-md hover:shadow-lg transition-shadow duration-300 rounded-lg">
                 <div>
-                    <h2 className="font-bold text-2xl">Topic Title</h2>
-                    <div> Topic Description</div>   
+                    <h2 className="font-semibold text-2xl text-slate-800 mb-1">Movie Title</h2>
+                    <div className="text-slate-600">Movie Description</div>
                 </div>
 
-                <div className="flex gap-2">
+                <div className="flex gap-3 items-center">
                     <RemoveBtn/>
-                    <Link href={'/editTopic/123'}>
-                        <HiPencilAlt size={24}/>
+                    <Link href='/editTopic/123'>
+                        <div className="bg-slate-200 p-2 rounded-full cursor-pointer hover:bg-slate-300 transition-colors duration-300">
+                            <HiPencilAlt className="text-slate-600 hover:text-slate-800" size={24} />
+                        </div>
                     </Link>
                 </div>
             </div>
         </>
-    )
+    );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,10 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-        <Navbar/>
-        {children}
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+      </head>
+      <body className={inter.className + " bg-gradient-to-br from-gray-100 to-white"}>
+        <div className="max-w-4xl mx-auto py-6 px-4 sm:px-6 lg:px-8 transition-all duration-500">
+          <Navbar/>
+          <main className="mt-6">
+            {children}
+          </main>
         </div>
       </body>
     </html>
