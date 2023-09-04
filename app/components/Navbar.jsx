3196b2dcb1d91f382c5c34e893180f4e4c20bb88
Norisama <gloriazbh@gmail.com>
2023-09-04 17:42:47 -0400

    style finished

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
       <nav className="flex justify-between items-center bg-slate-800 px-4 py-3">
         <Link className="text-white font-bold text-xl" href={"/"}>Home</Link>
-        <Link className="bg-white p-2" href={"/about"}>About</Link>
+        <Link className="bg-white p-2" href={"/addTopic"}>Add Topic</Link>
       </nav>
     );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
 import TopicsList from "./components/TopicsList";
 
 export default function Home() {
-  return <TopicsList />;
+  return (
+    <>
+      <TopicsList />
+      <TopicsList />
+      <TopicsList />
+    </>
+  )
 }
