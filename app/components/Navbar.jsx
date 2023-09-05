e45a0fbf290b1cea62e43d76baf86bbab75af654
Norisama <gloriazbh@gmail.com>
2023-09-05 01:47:21 -0400

    update NavBar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center px-6 py-3 shadow-md h-32"
-            style={{ backgroundImage: "url('/navbar-img.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}
+            style={{ backgroundImage: "url('/navbar-img.png')", backgroundSize: "cover", backgroundPosition: "center" }}
         >
             <Link className="text-white font-bold text-2xl hover:text-slate-400 transition-colors duration-300" href={"/"}>GG's Movie Magic</Link>
             <Link className="bg-slate-700 text-white p-2 rounded hover:bg-slate-600 transition-colors duration-300" href={"/addMovie"}>Add Movie</Link>
