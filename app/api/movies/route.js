ad84ac02c2bedf941ab3fed3f54f93a8e5d1a9fd
Norisama <gloriazbh@gmail.com>
2023-09-05 13:03:07 -0400

    update post and remove update in home route

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -7,29 +7,41 @@ export async function POST(request) {
     try {
         // Ensure MongoDB connection
         await connectMongoDB();
+
         // Parse the request body
-        const { movieName } = await request.json();
-        // Validation (can be extended based on your needs)
+        const { movieName, description } = await request.json();
+
+        // Validation for movieName
         if (!movieName) {
             return NextResponse.json({ message: "Movie name is required" }, { status: 400 });
         }
+
+        // Validation for description (assuming it's also required; remove this block if it's optional)
+        if (!description) {
+            return NextResponse.json({ message: "Description is required" }, { status: 400 });
+        }
+
         // Check if movie with the same name already exists
         const existingMovie = await Movie.findOne({ movieName });
         if (existingMovie) {
             return NextResponse.json({ message: "Movie with the same name already exists" }, { status: 409 });
         }
-        // Create a new movie
-        await Movie.create({ movieName });
+
+        // Create a new movie with both movieName and description
+        await Movie.create({ movieName, description });
+
         // Return a success response
         return NextResponse.json({ message: "Movie created successfully" }, { status: 201 });
+
     } catch (error) {
-        // Log the error (better logging can be implemented based on your needs)
+        // Log the error
         console.error("Error while creating movie:", error);
 
         // Return a server error response
         return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
     }
 }
+
 
 
 // GET all movies
@@ -49,46 +61,26 @@ export async function DELETE(request) {
     try {
         await connectMongoDB();
 
-        const { movieName } = await request.json();
+        // Extract the ID from the request
+        const id = request.nextUrl.searchParams.get("id");
 
-        if (!movieName) {
-            return NextResponse.json({ message: "Movie name is required" }, { status: 400 });
+        if (!id) {
+            return NextResponse.json({ message: "Movie ID is required" }, { status: 400 });
         }
 
-        const movie = await Movie.findOneAndDelete({ movieName });
+        // Use the findByIdAndDelete method to delete the movie by its ID
+        const movie = await Movie.findByIdAndDelete(id);
 
+        // If the movie wasn't found, return a 404 error
         if (!movie) {
             return NextResponse.json({ message: "Movie not found" }, { status: 404 });
         }
 
-        return NextResponse.json({ message: "Movie deleted successfully" });
+        // Return a success message
+        return NextResponse.json({ message: "Movie deleted successfully" }, { status: 200 });
 
     } catch (error) {
         console.error("Error while deleting movie:", error);
-        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
-    }
-}
-
-export async function UPDATE(request) {
-    try {
-        await connectMongoDB();
-
-        const { currentMovieName, newDetails } = await request.json();
-
-        if (!currentMovieName || !newDetails) {
-            return NextResponse.json({ message: "Both current movie name and new details are required" }, { status: 400 });
-        }
-
-        const movie = await Movie.findOneAndUpdate({ movieName: currentMovieName }, newDetails, { new: true });
-
-        if (!movie) {
-            return NextResponse.json({ message: "Movie not found" }, { status: 404 });
-        }
-
-        return NextResponse.json({ message: "Movie updated successfully", data: movie });
-
-    } catch (error) {
-        console.error("Error while updating movie:", error);
         return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
     }
 }
--- models/movie.js
+++ models/movie.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const MovieSchema = new Schema({
     movieName:String,
-    description:String
+    description:String,
 });
 
 const Movie = mongoose.models.Movie || mongoose.model("Movie", MovieSchema);
