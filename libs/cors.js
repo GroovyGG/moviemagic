2e185e533e43c2f65d54157b8ac827bf182b207d
Norisama <gloriazbh@gmail.com>
2023-09-06 01:30:40 -0400

    fix bug in cors.js

--- libs/cors.js
+++ libs/cors.js
@@ -1,6 +1,6 @@
 // cors.js
 function setCorsHeaders(req, res) {
-    const allowedOrigins = ["https://movie.bihanzhu.com", "https://yourOtherDomain.com","http://localhost:3000"];
+    const allowedOrigins = ["https://movie.bihanzhu.com", "https://moviemagic-eosin.vercel.app","http://localhost:3000"];
     const origin = req.headers.origin;
 
     if (allowedOrigins.includes(origin)) {
